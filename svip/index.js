3fbd25d9a77311682a02081b38fb4aa39d123e0a
hlkitty <hlkitty@yeah.net>
2020-04-13 15:39:32 +0800

    add some fix spurl

--- svip/index.js
+++ svip/index.js
@@ -17,65 +17,7 @@
 		}
 		return "";
 	}
-	var vipUrls = {
-		"1907": "https://z1.m1907.cn/?jx=",
-		"音萌": "https://api.v6.chat/?url=",
-		"heimijx": "https://www.heimijx.com/jx/api/?url=",
-		"流氓凡": "https://jx.wslmf.com/?url=",
-		"47Player": "https://api.47ks.com/webcloud/?v=",
-		"石头解析": "https://jiexi.071811.cc/jx.php?url=",
-		"BL解析": "https://vip.bljiex.com/?v=",
-		"人人解析": "https://vip.mpos.ren/v/?url=",
-		"乐乐云": "https://660e.com/?url=",
-		"yangju": "https://cdn.yangju.vip/k/?url=",
-		"思古": "https://api.sigujx.com/?url=",
-		"简傲云": "https://vip.jaoyun.com/index.php?url=",
-		"myxin": "https://www.myxin.top/jx/api/?url=",
-		"60jx": "https://60jx.com/?url=",
-		"小蒋极致": "https://www.kpezp.cn/jlexi.php?url=",
-		"黑云": "https://jiexi.380k.com/?url=",
-		"ivito": "https://jx.ivito.cn/?url=",
-		"927": "https://api.927jx.com/vip/?url=",
-		"920": "https://api.tv920.com/vip/?url=",
-		"lhh": "https://api.lhh.la/vip/?url=",
-		"8b": "https://api.8bjx.cn/?url=",
-		"41": "https://jx.f41.cc/?url=",
-		"ckmov": "https://www.ckmov.vip/api.php?url=",
-		"517": "https://cn.bjbanshan.cn/jx.php?url=",
-		"ha12": "https://py.ha12.xyz/sos/index.php?url=",
-		"mw0": "https://jx.mw0.cc/?url=",
-		"1ff1": "https://jx.1ff1.cn/?url=",
-		"180": "https://jx.000180.top/jx/?url=",
-		"okjs": "https://okjx.cc/?url=",
-		"百度穷二代": "http://jx.ejiafarm.com/dy.php?url=",
-		"金桥解析": "http://jqaaa.com/jx.php?url=",
-		"艾闪闪": "http://api.zncss.com/?url=",
-		"598110": "http://jx.598110.com/?url=",
-		"jlsprh": "http://vip.jlsprh.com/?url=",
-		"618阁": "http://jx.618ge.com/?url=",
-		"DuPlay": "http://jx.du2.cc/?url=",
-		"618G": "http://jx.618g.com/?url=",
-		"大亨影院解析": "http://jx.cesms.cn/?url=",
-		"百域阁": "http://api.baiyug.vip/?url=",
-		"1717云": "http://www.1717yun.com/jx/vip?url=",
-		"163人": "http://jx.api.163ren.com/vod.php?url=",
-		"花园解析": "http://j.zz22x.com/jx/?url=",
-	};
-	var turl = window.location.hash.slice(1);
-	if (turl.slice(0, 4) != "http") {
-		turl = window.location.search.slice(1);
-	}
-	if (turl.slice(0, 4) != "http") {
-		turl = "";
-	}
-	var url = decodeURIComponent(turl);
-	while (url && url != turl) {
-		turl = url;
-		url = decodeURIComponent(turl);
-	}
-	if (url && (url.slice(0, 7) != "http://") && (url.slice(0, 8) != "https://")) {
-		url = "";
-	}
+	
 	function loadFrame(sourceurl) {
 		var view = document.getElementById("viewframe");
 		view.src = sourceurl;
@@ -132,9 +74,34 @@
 		floatdiv.onmousedown = startmove;
 		floatdiv.onmouseup = stopmove;
 		floatdiv.onmouseenter = viewfloatdiv;
-		floatdiv.onmouseleave = unviewfloatdiv;
-		unviewfloatdiv();
-		//var durl = encodeURIComponent(url);
+		floatdiv.onmouseleave = unviewfloatdiv;		
+		for (var i in spUrls) {
+			var spUrl = spUrls[i][1];
+			var httpTarget = "_blank";
+			var item = document.createElement("a");
+			item.setAttribute("href", spUrl);
+			item.setAttribute("target", httpTarget);
+			item.innerHTML = spUrls[i][0];
+			floatdiv.appendChild(item);
+			var textNode = document.createTextNode("	");
+			floatdiv.appendChild(textNode);
+		}
+		unviewfloatdiv();		
+		var turl = window.location.hash.slice(1);
+		if (turl.slice(0, 4) != "http") {
+			turl = window.location.search.slice(1);
+		}
+		if (turl.slice(0, 4) != "http") {
+			turl = "";
+		}
+		var url = decodeURIComponent(turl);
+		while (url && url != turl) {
+			turl = url;
+			url = decodeURIComponent(turl);
+		}
+		if (url && (url.slice(0, 7) != "http://") && (url.slice(0, 8) != "https://")) {
+			url = "";
+		}
 		if (!url) {
 			loadFrame("time.html");
 			return;
@@ -143,8 +110,8 @@
 		var initUrl = getCookie("url");
 		var containsUrl = false;
 		var firstItem,firstUrl;
-		for (var urlKey in vipUrls) {
-			var vipUrl = vipUrls[urlKey];
+		for (var i in vipUrls) {
+			var vipUrl = vipUrls[i][1];
 			var httpTarget = "viewframe";
 			var useBlank = window.location.protocol == "https:" && vipUrl.startsWith("http:");
 			var item = document.createElement("a");
@@ -175,10 +142,9 @@
 					}
 				};
 			})({ url: vipUrl, useBlank: useBlank });
-			//item.setAttribute("href","javascript:void(0);");
 			item.setAttribute("href", vipUrl + durl);
 			item.setAttribute("target", httpTarget);
-			item.innerHTML = urlKey;
+			item.innerHTML = vipUrls[i][0];
 			contentdiv.appendChild(item);
 			var textNode = document.createTextNode("	");
 			contentdiv.appendChild(textNode);
@@ -189,5 +155,63 @@
 		}
 		loadFrame(initUrl + durl);
 	}
-	window.onload = InitDiv;
+	window.onload = InitDiv;	
+	var vipUrls = [
+		["1907", "https://z1.m1907.cn/?jx="],
+		["音萌", "https://api.v6.chat/?url="],
+		["heimijx", "https://www.heimijx.com/jx/api/?url="],
+		["流氓凡", "https://jx.wslmf.com/?url="],
+		["47Player", "https://api.47ks.com/webcloud/?v="],
+		["石头解析", "https://jiexi.071811.cc/jx.php?url="],
+		["BL解析", "https://vip.bljiex.com/?v="],
+		["人人解析", "https://vip.mpos.ren/v/?url="],
+		["乐乐云", "https://660e.com/?url="],
+		["yangju", "https://cdn.yangju.vip/k/?url="],
+		["思古", "https://api.sigujx.com/?url="],
+		["简傲云", "https://vip.jaoyun.com/index.php?url="],
+		["myxin", "https://www.myxin.top/jx/api/?url="],
+		["60jx", "https://60jx.com/?url="],
+		["小蒋极致", "https://www.kpezp.cn/jlexi.php?url="],
+		["黑云", "https://jiexi.380k.com/?url="],
+		["ivito", "https://jx.ivito.cn/?url="],
+		["927", "https://api.927jx.com/vip/?url="],
+		["920", "https://api.tv920.com/vip/?url="],
+		["lhh", "https://api.lhh.la/vip/?url="],
+		["8b", "https://api.8bjx.cn/?url="],
+		["41", "https://jx.f41.cc/?url="],
+		["ckmov", "https://www.ckmov.vip/api.php?url="],
+		["517", "https://cn.bjbanshan.cn/jx.php?url="],
+		["ha12", "https://py.ha12.xyz/sos/index.php?url="],
+		["mw0", "https://jx.mw0.cc/?url="],
+		["1ff1", "https://jx.1ff1.cn/?url="],
+		["180", "https://jx.000180.top/jx/?url="],
+		["okjs", "https://okjx.cc/?url="],
+		["百度穷二代", "http://jx.ejiafarm.com/dy.php?url="],
+		["金桥解析", "http://jqaaa.com/jx.php?url="],
+		["艾闪闪", "http://api.zncss.com/?url="],
+		["598110", "http://jx.598110.com/?url="],
+		["jlsprh", "http://vip.jlsprh.com/?url="],
+		["618阁", "http://jx.618ge.com/?url="],
+		["DuPlay", "http://jx.du2.cc/?url="],
+		["618G", "http://jx.618g.com/?url="],
+		["大亨影院解析", "http://jx.cesms.cn/?url="],
+		["百域阁", "http://api.baiyug.vip/?url="],
+		["1717云", "http://www.1717yun.com/jx/vip?url="],
+		["163人", "http://jx.api.163ren.com/vod.php?url="],
+		["花园解析", "http://j.zz22x.com/jx/?url="],
+	];
+	var spUrls = [
+		["M1907", "https://z1.m1907.cn/"],
+        ["电影盒子", "https://www.dyhz8.com/"],
+        ["TV6", "https://www.tv6.com/"],
+        ["百汇", "http://www.ibhdy.com/"],
+        ["高乐播", "http://www.gaolebo.com/"],
+        ["迅雷影院", "https://www.xunleiyy.com/"],
+        ["云播TV", "https://www.yunbtv.com/"],
+        ["高清云影视", "https://www.gqytv.com/"],
+        ["豆瓣资源", "http://www.doubanzy.com/"],
+        ["最大", "http://zuidazy.net/"],
+        ["片库", "http://pianku.tv/"],
+        ["1090", "http://1090ys.com/"],
+	];
 })();
