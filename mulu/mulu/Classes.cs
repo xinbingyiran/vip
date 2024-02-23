c012bb0efb656497953165b5978fe3151f28646b
xinbingyiran <xinbingyiran@qq.com>
2024-02-23 14:06:13 +0800

    add mulu

---
+++ mulu/mulu/Classes.cs
@@ -0,0 +1,237 @@
+﻿using System.Text;
+using System.Text.Json.Serialization;
+
+namespace mulu;
+
+
+public class Game
+{
+    public string? Code { get; set; }
+    public string? Name { get; set; }
+    public string? MMSS { get; set; }
+    public string? Describe { get; set; }
+    public string? RLzz { get; set; }
+    public string? Types { get; set; }
+    public string? YXBB { get; set; }
+    public string? Addr1 { get; set; }
+    public string? Addr2 { get; set; }
+    public string? Addr3 { get; set; }
+}
+
+[JsonSourceGenerationOptions(WriteIndented =true)]
+[JsonSerializable(typeof(Sys_content_version))]
+[JsonSerializable(typeof(Sys_content_more))]
+[JsonSerializable(typeof(Game[]))]
+internal partial class SYS : JsonSerializerContext
+{
+}
+public class Sys_content_more
+{
+    public long Id { get; set; }
+
+    public string? BH { get; set; }
+
+    public string? MM { get; set; }
+
+    public string? Name1 { get; set; }
+
+    public string? Name2 { get; set; }
+
+    public string? BiaoQ { get; set; }
+
+    public string? XingJ { get; set; }
+
+    public string? MageA { get; set; }
+
+    public string? RongL { get; set; }
+
+    public string? RiQ { get; set; }
+
+    public string? Category { get; set; }
+}
+public class Sys_content_version
+{
+    public string? Version { get; set; }
+
+    public List<Sys_content_more>? Content { get; set; }
+}
+public class Ini
+{
+
+    public static Encoding Encoding { get; set; } = Encoding.GetEncoding("gbk");
+
+    public Dictionary<string, Dictionary<string, object>> Data { get; set; } = new ();
+
+    public static Ini ParseString(string context)
+    {
+        Ini ini = new();
+        var array = context.Split('\n');
+        string? text = null;
+        foreach (string? text2 in array)
+        {
+            if (string.IsNullOrEmpty(text2))
+            {
+                continue;
+            }
+            string text3 = text2.Trim();
+            if (IsAnnotation(text3))
+            {
+                continue;
+            }
+            if (text3.StartsWith('[') && text3.EndsWith(']') && text3.Length >= 3)
+            {
+                text = text3[1..^1];
+                continue;
+            }
+            int num = text3.IndexOf('=');
+            if (num > 0 && num + 1 < text3.Length && text != null)
+            {
+                string key = text3[..num].Trim();
+                string value = text3[(num + 1)..].Trim();
+                ini.Add(text, key, value);
+            }
+        }
+        return ini;
+    }
+
+    private string? GetString(string section, string key)
+    {
+        //Discarded unreachable code: IL_0005
+        //IL_0006: Incompatible stack heights: 0 vs 1
+        try
+        {
+            return Data[section][key].ToString();
+        }
+        catch
+        {
+            //Console.WriteLine("获取值失败:section = " + section + ", key = " + key + " " + ex.Message);
+            return null;
+        }
+    }
+
+    public string GetStringValue(string section, string key, string defaultValue = "")
+    {
+        //Discarded unreachable code: IL_0005
+        //IL_0006: Incompatible stack heights: 0 vs 1
+        string? text = GetString(section, key);
+        if (string.IsNullOrEmpty(text))
+        {
+            text = defaultValue;
+        }
+        return text;
+    }
+
+    public bool GetBooleanValue(string section, string key, bool defaultValue)
+    {
+        //Discarded unreachable code: IL_0005
+        //IL_0006: Incompatible stack heights: 0 vs 1
+        if (bool.TryParse(GetString(section, key), out var result))
+        {
+            return result;
+        }
+        return defaultValue;
+    }
+
+    public int GetIntegerValue(string section, string key, int defaultValue = 0)
+    {
+        //Discarded unreachable code: IL_0005
+        //IL_0006: Incompatible stack heights: 0 vs 1
+        if (int.TryParse(GetString(section, key), out var result))
+        {
+            return result;
+        }
+        return defaultValue;
+    }
+
+    public double GetDoubleValue(string section, string key, double defaultValue = 0.0)
+    {
+        //Discarded unreachable code: IL_0005
+        //IL_0006: Incompatible stack heights: 0 vs 1
+        if (double.TryParse(GetString(section, key), out var result))
+        {
+            return result;
+        }
+        return defaultValue;
+    }
+
+    public float GetFloatValue(string section, string key, float defaultValue = 0f)
+    {
+        //Discarded unreachable code: IL_0005
+        //IL_0006: Incompatible stack heights: 0 vs 1
+        if (float.TryParse(GetString(section, key), out var result))
+        {
+            return result;
+        }
+        return defaultValue;
+    }
+
+    public short GetShortValue(string section, string key, short defaultValue = 0)
+    {
+        //Discarded unreachable code: IL_0005
+        //IL_0006: Incompatible stack heights: 0 vs 1
+        if (short.TryParse(GetString(section, key), out var result))
+        {
+            return result;
+        }
+        return defaultValue;
+    }
+
+    public long GetLongValue(string section, string key, long defaultValue = 0L)
+    {
+        //Discarded unreachable code: IL_0005
+        //IL_0006: Incompatible stack heights: 0 vs 1
+        if (long.TryParse(GetString(section, key), out var result))
+        {
+            return result;
+        }
+        return defaultValue;
+    }
+
+    public byte GetByteValue(string section, string key, byte defaultValue = 0)
+    {
+        //Discarded unreachable code: IL_0005
+        //IL_0006: Incompatible stack heights: 0 vs 1
+        if (byte.TryParse(GetString(section, key), out var result))
+        {
+            return result;
+        }
+        return defaultValue;
+    }
+
+    private static bool IsAnnotation(string line)
+    {
+        if (line != null && line.Length != 0 && !line.StartsWith(';'))
+        {
+            return line.StartsWith('#');
+        }
+        return true;
+    }
+
+    public void Add(string section, string key, object value)
+    {
+        if (!Data.ContainsKey(section) || Data[section] == null)
+        {
+            Data[section] = new Dictionary<string, object>();
+        }
+        if (value != null && section != null && key != null)
+        {
+            Data[section][key] = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (KeyValuePair<string, Dictionary<string, object>> datum in Data)
+        {
+            stringBuilder.Append("[").Append(datum.Key).Append("]\r\n");
+            foreach (KeyValuePair<string, object> item in datum.Value)
+            {
+                stringBuilder.Append(item.Key).Append(" = ").Append(item.Value)
+                    .Append("\r\n");
+            }
+            stringBuilder.Append("\r\n");
+        }
+        return stringBuilder.ToString();
+    }
+}
---
+++ mulu/mulu/Program.cs
@@ -0,0 +1,222 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+namespace mulu;
+public class Program
+{
+
+    private static Encoding? Encoding;
+    private static readonly ICryptoTransform des = DES.Create().CreateDecryptor(Encoding.UTF8.GetBytes("consmkey"), new byte[] { 18, 52, 86, 120, 144, 171, 205, 239 });
+    private static readonly string h = "wg.MtbH&zvqS^!(d";
+    private static readonly string c = "PTU18vZQEQka6CE2TOvwU/jsNrqWiGxPLm9u8oyznLFZbPOHTANpHM0yDKcB4J/bc6OS2RY2MuIKZS40b1ROZ+DbwG15s/m6ACXvz7L0Bnx5CyHuptfB+sR0JK3ljmsHlFDi2jh59r+pGNHho+st8AyS6ORMI/fNtTgOSUk8xTYHouvOdINg2EYdAuZaWttngunBnpawqYPmQQ+T0TYHtg==";
+    private static readonly string f = "CWxXAmuJ0+QfAzurL4R8qZF5nUJq1YSyCO38gIlExuh66ZGv1k0De1yzZh13+agfe8oop8cS6UlsAK7DIHBbx8UMovijNBjiA/U2AuFNwbQ=";
+    private static string? cstr;
+    private static byte fb;
+    private static readonly string zxxzdz1 = "https://m.189.ly93.cc/share/";
+    private static readonly HttpClient _client = new();
+    private static string Decrypt(string toDecrypt)
+    {
+        if (string.IsNullOrEmpty(toDecrypt))
+        {
+            return toDecrypt;
+        }
+        try
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(h);
+            byte[] array = Convert.FromBase64String(toDecrypt);
+            var aes = Aes.Create();
+            aes.Key = bytes;
+            var bytes2 = aes.DecryptEcb(array, PaddingMode.PKCS7);
+            return Encoding.UTF8.GetString(bytes2);
+        }
+        catch (Exception)
+        {
+            return toDecrypt;
+        }
+    }
+    public static string? DESDecrypt(string? decryptString)
+    {
+        if (string.IsNullOrEmpty(decryptString))
+        {
+            return decryptString;
+        }
+        try
+        {
+            byte[] array = Convert.FromBase64String(decryptString);
+            using var ms = new MemoryStream();
+            using var cs = new CryptoStream(ms, des, CryptoStreamMode.Write);
+            cs.Write(array, 0, array.Length);
+            cs.FlushFinalBlock();
+            return Encoding.UTF8.GetString(ms.ToArray());
+        }
+        catch
+        {
+            return decryptString;
+        }
+    }
+    public static async Task<string> DecodecAsync(string path, byte password)
+    {
+        string result = string.Empty;
+        try
+        {
+            byte[] bytes;
+            if (path.StartsWith("http"))
+            {
+                bytes = await _client.GetByteArrayAsync(path);
+            }
+            else
+            {
+                bytes = await File.ReadAllBytesAsync(path);
+            }
+            result = Encoding!.GetString(bytes.Select(e => (byte)(e ^ password)).ToArray());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("解密失败: " + ex.Message);
+        }
+        return result;
+    }
+    private static void Init()
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        Encoding = Encoding.GetEncoding("GBK");
+
+
+        cstr = Decrypt(Decrypt(Decrypt(Decrypt(c))));
+        fb = (byte)int.Parse(Decrypt(Decrypt(Decrypt(Decrypt(f)))));
+        Console.WriteLine(cstr);
+        Console.WriteLine(fb);
+
+    }
+    private static async Task jiancha(Ini ini)
+    {
+        try
+        {
+            string text = zxxzdz1 + "list";
+            if ((await _client.GetAsync(text)).IsSuccessStatusCode)
+            {
+                var ZXXZ_To = ini.GetStringValue("C3", "ZaiXian_To", "WLCW");
+                var toPath = ZXXZ_To + "To.txt";
+                Console.WriteLine(toPath);
+                string stringValue = Ini.ParseString(await _client.GetStringAsync(toPath)).GetStringValue("To", "On1", "WLCW");
+                Console.WriteLine("------------------------------------------------------------------------");
+                //Console.WriteLine(stringValue);
+                await File.WriteAllTextAsync("to.txt", stringValue);
+                Console.WriteLine("------------------------------------------------------------------------");
+            }
+        }
+        catch (HttpRequestException)
+        {
+        }
+    }
+
+
+    public static Game[] GetAllData(List<Sys_content_more> contentList)
+    {
+        return contentList.Select(p =>
+            new Game
+            {
+                Code = DESDecrypt(p.BH),
+                MMSS = DESDecrypt(p.MM),
+                Describe = DESDecrypt(p.Name2),
+                Name = DESDecrypt(p.Name1),
+                RLzz = DESDecrypt(p.RongL),
+                Types = DESDecrypt(p.BiaoQ)
+            }
+        ).ToArray();
+    }
+
+    public static async Task huoquyx(Game game, string tt1a002A)
+    {
+        var ini1z = Ini.ParseString(await DecodecAsync($"{tt1a002A}{game.Code}.txt", fb));
+        string yxbb = ini1z.GetStringValue("zhu", "yxbb", "WLCW");
+        game.YXBB = yxbb == "WLCW" ? null : yxbb;
+        string xiazai1_ming = ini1z.GetStringValue("xiazai", "xiazai1_ming", "WLCW");
+        string xiazai1_dizhi = ini1z.GetStringValue("xiazai", "xiazai1_dizhi", "WLCW");
+        string xiazai1_fwm = ini1z.GetStringValue("xiazai", "xiazai1_fwm", "WLCW");
+        game.Addr1 = xiazai1_ming == "WLCW" ? null : $"{xiazai1_ming}: {xiazai1_dizhi}{(xiazai1_fwm == "WLCW" ? "" : $"#{xiazai1_fwm}")}";
+        string xiazai2_ming = ini1z.GetStringValue("xiazai", "xiazai2_ming", "WLCW");
+        string xiazai2_dizhi = ini1z.GetStringValue("xiazai", "xiazai2_dizhi", "WLCW");
+        string xiazai2_fwm = ini1z.GetStringValue("xiazai", "xiazai2_fwm", "WLCW");
+        game.Addr2 = xiazai2_ming == "WLCW" ? null : $"{xiazai2_ming}: {xiazai2_dizhi}{(xiazai2_fwm == "WLCW" ? "" : $"#{xiazai2_fwm}")}";
+        string xiazai3_ming = ini1z.GetStringValue("xiazai", "xiazai3_ming", "WLCW");
+        string xiazai3_dizhi = ini1z.GetStringValue("xiazai", "xiazai3_dizhi", "WLCW");
+        string xiazai3_fwm = ini1z.GetStringValue("xiazai", "xiazai3_fwm", "WLCW");
+        game.Addr3 = xiazai3_ming == "WLCW" ? null : $"{xiazai3_ming}: {xiazai3_dizhi}{(xiazai3_fwm == "WLCW" ? "" : $"#{xiazai3_fwm}")}";
+    }
+
+    public static async Task Main()
+    {
+        var serial = new SYS(new(JsonSerializerDefaults.Web)
+        {
+            WriteIndented = true,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingDefault,
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        });
+        Init();
+
+        Console.WriteLine(cstr + "a/a.txt");
+        var str = await DecodecAsync(cstr + "a/a.txt", fb);
+        Console.WriteLine("------------------------------------------------------------------------");
+        await File.WriteAllTextAsync("a.txt", str);
+
+        //Console.WriteLine(str);
+        Console.WriteLine("------------------------------------------------------------------------");
+
+        Ini ini = Ini.ParseString(str);
+        var tt1a001 = ini.GetStringValue("zhu", "wj", "WLCW");
+        var tt1a002A = ini.GetStringValue("zhu", "shuju1", "WLCW");
+        await jiancha(ini);
+
+        var wjurl = tt1a001 + "WenJian.json";
+        Console.WriteLine(wjurl);
+        var wjtext = await _client.GetStringAsync(wjurl);
+        Console.WriteLine("------------------------------------------------------------------------");
+        var ct = JsonSerializer.Deserialize(wjtext, serial.Sys_content_version)!;
+        var datas = GetAllData(ct.Content!);
+        await File.WriteAllTextAsync("Game.json", JsonSerializer.Serialize(datas, serial.GameArray));
+        Console.WriteLine("------------------------------------------------------------------------");
+
+        var l = datas.Length;
+        var i = 0;
+        var time = DateTime.Now;
+        var suc = 0;
+        var fai = 0;
+        await Parallel.ForEachAsync(datas, async (g, t) =>
+        {
+            var retry = 0;
+            var c = Interlocked.Increment(ref i);
+            while (retry < 3)
+            {
+                retry++;
+                try
+                {
+                    await huoquyx(g, tt1a002A);
+                }
+                catch
+                {
+                }
+                if (!string.IsNullOrEmpty(g.Addr1) || !string.IsNullOrEmpty(g.Addr2) || !string.IsNullOrEmpty(g.Addr3))
+                {
+                    break;
+                }
+            }
+            if (!string.IsNullOrEmpty(g.Addr1) || !string.IsNullOrEmpty(g.Addr2) || !string.IsNullOrEmpty(g.Addr3))
+            {
+                Interlocked.Increment(ref suc);
+            }
+            else
+            {
+                Interlocked.Increment(ref fai);
+            }
+            if (DateTime.Now - time > TimeSpan.FromSeconds(3))
+            {
+                time = DateTime.Now;
+                Console.WriteLine($"链接获取：获取成功 {suc} / {l} ,失败 {fai} / {l} 。");
+            }
+        });
+        Console.WriteLine($"获取完成：获取成功 {suc} / {l} ,失败 {fai} / {l} 。");
+        await File.WriteAllTextAsync("GameAll.json", JsonSerializer.Serialize(datas, serial.GameArray));
+        Console.WriteLine("------------------------------------------------------------------------");
+    }
+}
