2ba6161b03e7a8333c03605cfcd91058dd2997dd
xinbingyiran <xinbingyiran@qq.com>
2026-03-09 12:06:55 +0800

    update game and douyin script

--- js/douyin.js
+++ js/douyin.js
@@ -1,29 +1,52 @@
 !function () {
     if (globalThis._douyin_) {
-        globalThis._douyin_.stoped = false;
+        globalThis._douyin_.start();
         return;
     }
     const douyin = globalThis._douyin_ = {
         stoped: false,
-        log: (message) => console.info(message)
+        log: (msg) => console.info(`%c %s`, 'color: green; font-size: 2em;', msg),
+        get liveRoom() {
+
+            //[data-e2e=feed-active-video]
+            return globalThis.document.querySelector("main.live-main video[autoplay]");
+        },
+        get slideVideo() {
+            //[data-e2e=live-slider]
+            return globalThis.document.querySelector("#slidelist[data-active=true] video[autoplay]");
+        }
     };
     douyin.addScript = (url, useCallback) => {
-        var script = globalThis.document.createElement('script');
-        script.setAttribute('type', 'text/javascript'), script.setAttribute('src', url), script.onload = useCallback, globalThis.document.getElementsByTagName('head')[0].appendChild(script);
+        const script = globalThis.document.createElement('script');
+        script.setAttribute('type', 'text/javascript');
+        script.setAttribute('src', url);
+        script.onload = useCallback;
+        globalThis.document.getElementsByTagName('head')[0].appendChild(script);
     }
     douyin.checkAction = (keyItem, ts) => {
-        if (!keyItem.check()) {
+        const ci = keyItem.check;
+        if (ci != keyItem._actionCI) {
+            keyItem._actionCI = ci;
+            if (keyItem._actions?.length && typeof douyin.log == "function") {
+                douyin.log(`【取消】${keyItem._actions[0].type} ${keyItem.key}【${keyItem.code}】`);
+            }
             keyItem._actions = [];
+        }
+        if (!keyItem._actionCI) {
             return;
         }
         const actions = keyItem._actions ??= [];
         if (actions.length == 0) {
             let downTs = ts + keyItem.upMS + Math.random() * keyItem.upMSRand;
-            actions.push({
+            const fa = {
                 type: 'keydown',
                 ts: downTs,
                 repeat: false
-            });
+            };
+            actions.push(fa);
+            if (typeof douyin.log == "function") {
+                douyin.log(`【部署】${fa.type} ${keyItem.key}【${keyItem.code}】 延迟: ${~~(downTs - ts)}`);
+            }
             const upTs = downTs + keyItem.downMs + Math.random() * keyItem.downMSRand;
             downTs += keyItem.repeatDelay;
             while (downTs < upTs) {
@@ -34,11 +57,7 @@
                 });
                 downTs += keyItem.repeatRate;
             }
-            actions.push({
-                type: 'keyup',
-                ts: upTs,
-                repeat: false
-            });
+            actions.push({ type: 'keyup', ts: upTs, repeat: false });
         }
         else if (ts > actions[0].ts) {
             const currentAction = actions.shift();
@@ -50,14 +69,15 @@
                 bubbles: true
             };
             try {
-                globalThis.document.body.dispatchEvent(new KeyboardEvent(currentAction.type, eventInit));
+                const dv = new KeyboardEvent(currentAction.type, eventInit);
+                globalThis.document.body.dispatchEvent(dv);
                 if (typeof douyin.log == "function") {
-                    douyin.log(`${currentAction.type} key:${keyItem.key} code:${keyItem.code}`);
+                    douyin.log(`【触发】${currentAction.type} ${keyItem.key}【${keyItem.code}】`);
                 }
             }
             catch (e) {
                 if (typeof douyin.log == "function") {
-                    douyin.log(`${currentAction.type} key:${keyItem.key} code:${keyItem.code} 【Error】${e}`);
+                    douyin.log(`【异常】${currentAction.type} ${keyItem.key}【${keyItem.code}】 异常: ${e}`);
                 }
             }
         }
@@ -65,16 +85,22 @@
 
     douyin.mainLoop = (ts) => {
         if (douyin.stoped) {
-            for (const keyItem of douyin.items) { delete keyItem._actions; }
+            for (const keyItem of douyin.items) {
+                delete keyItem._actions;
+            }
             return;
         }
-        for (const keyItem of douyin.items) { douyin.checkAction(keyItem, ts); }
+        for (const keyItem of douyin.items) {
+            douyin.checkAction(keyItem, ts);
+        }
         requestAnimationFrame(douyin.mainLoop);
     }
 
     //# 间断性点按 ArrowDown
     douyin.keyArrowDown = {
-        check: () => !globalThis.document.querySelector("main.live-main video[autoplay]") && !!globalThis.document.querySelector("#slidelist[data-active=true] video[autoplay]"),// || [data-e2e=feed-active-video]  || [data-e2e=live-slider]
+        get check() {
+            return douyin.liveRoom ? null : douyin.slideVideo;
+        },
         key: "ArrowDown",
         code: "ArrowDown",
         keyCode: 40,
@@ -83,13 +109,16 @@
         downMs: 200,
         downMSRand: 200,
         get upMS() {
-            const e = globalThis.document.querySelector("#slidelist[data-active=true] video[autoplay]").buffered; //  [data-e2e=live-slider]
-            if (e.length == 0) {
-                return 500 + Math.random() * 3000;
+            const v = douyin.slideVideo;
+            if (!v) {
+                return 100;
+            }
+            const e = v.duration;
+            if (isFinite(e)) { // Infinity is live
+                return Math.min(2000 + Math.random() * 20000, e * (Math.random() * 200 + 500));
             }
             else {
-                const endTs = e.end(0);
-                return Math.min(2000 + Math.random() * 20000, endTs * 500 + Math.random() * endTs * 200);
+                return 2000 + Math.random() * 3000;
             }
         },
         upMSRand: 0
@@ -97,7 +126,9 @@
 
     // 间断性长按z
     douyin.keyZ = {
-        check: () => !!globalThis.document.querySelector("main.live-main video[autoplay]") && !globalThis.document.querySelector("#slidelist[data-active=true] video[autoplay]"),
+        get check() {
+            return douyin.slideVideo ? null : douyin.liveRoom;
+        },
         key: "z",
         code: "KeyZ",
         keyCode: 90,
@@ -119,7 +150,10 @@
     }
 
     //控制台
-    douyin.items = [douyin.keyArrowDown, douyin.keyZ]
+    douyin.items = [
+        douyin.keyArrowDown,
+        douyin.keyZ
+    ];
 
     //执行
     douyin.start();
